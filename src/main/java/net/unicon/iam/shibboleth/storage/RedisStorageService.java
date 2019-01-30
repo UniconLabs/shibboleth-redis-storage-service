d3bfb3ad097906678c8c9d84ff19ec7d805adbf7
Jj! <jj@unicon.net>
2019-01-30 12:57:06 -0600

    update redisson version initial WIP implementation

--- src/main/java/net/unicon/iam/shibboleth/storage/RedisStorageService.java
+++ src/main/java/net/unicon/iam/shibboleth/storage/RedisStorageService.java
@@ -1,18 +1,14 @@
 package net.unicon.iam.shibboleth.storage;
 
 
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import net.shibboleth.utilities.java.support.collection.Pair;
-import net.unicon.iam.shibboleth.storage.jackson.StorageRecordDeserializer;
-import net.unicon.iam.shibboleth.storage.jackson.StorageRecordSerializer;
 import org.opensaml.storage.AbstractStorageService;
 import org.opensaml.storage.MutableStorageRecord;
 import org.opensaml.storage.StorageRecord;
 import org.opensaml.storage.VersionMismatchException;
+import org.redisson.api.RLock;
 import org.redisson.api.RMapCache;
 import org.redisson.api.RedissonClient;
-import org.redisson.client.codec.Codec;
-import org.redisson.codec.JsonJacksonCodec;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -24,15 +20,6 @@ public class RedisStorageService extends AbstractStorageService {
 
     public RedisStorageService(final RedissonClient client) {
         this.client = client;
-
-        Codec codec = this.client.getConfig().getCodec();
-        if (codec instanceof JsonJacksonCodec) {
-            JsonJacksonCodec jsonJacksonCodec = (JsonJacksonCodec)codec;
-            SimpleModule module = new SimpleModule();
-            module.addDeserializer(StorageRecord.class, new StorageRecordDeserializer());
-            module.addSerializer(StorageRecord.class, new StorageRecordSerializer());
-            jsonJacksonCodec.getObjectMapper().registerModule(module);
-        }
     }
 
     private long getSystemExpiration(Long expiration) {
@@ -42,9 +29,10 @@ public class RedisStorageService extends AbstractStorageService {
     @Override
     public boolean create(@Nonnull String context, @Nonnull String key, @Nonnull String value, @Nullable Long expiration) throws IOException {
         RMapCache<String, StorageRecord> map = client.getMapCache(context);
-        StorageRecord storageRecord = new MutableStorageRecord(value, expiration);
+        StorageRecord storageRecord = new VersionMutableStorageRecord(value, expiration, 1L);
         if (expiration != null) {
-            return map.fastPutIfAbsent(key, storageRecord, getSystemExpiration(expiration), TimeUnit.MILLISECONDS);
+            long ttl = getSystemExpiration(expiration);
+            return map.fastPutIfAbsent(key, storageRecord, ttl, TimeUnit.MILLISECONDS);
         } else {
             return map.fastPutIfAbsent(key, storageRecord);
         }
@@ -60,38 +48,99 @@ public class RedisStorageService extends AbstractStorageService {
     @Nonnull
     @Override
     public Pair<Long, StorageRecord> read(@Nonnull String context, @Nonnull String key, long version) throws IOException {
-        return null;
+        RMapCache<String, StorageRecord> map = client.getMapCache(context);
+        StorageRecord storageRecord = read(context, key);
+        if (storageRecord == null) {
+            return new Pair<>();
+        }
+        if ((Long)version != null && version == storageRecord.getVersion()) {
+            return new Pair<>(version, null);
+        }
+        return new Pair<>(storageRecord.getVersion(), storageRecord);
+    }
+
+    private Long doUpdate(final Long version, final String context, final String key, final String value, final Long expiration) throws IOException {
+        final RLock lock = this.client.getLock(context + ":" + key);
+        lock.lock(10, TimeUnit.SECONDS);
+        try {
+            MutableStorageRecord record = (MutableStorageRecord) this.read(context, key);
+            if (record == null) {
+                return null;
+            }
+
+            if (version != null && version != record.getVersion()) {
+                throw new VersionMismatchWrapperException(new VersionMismatchException());
+            }
+
+            if (value != null) {
+                record.setValue(value);
+                record.incrementVersion();
+            }
+
+            record.setExpiration(expiration);
+
+            if (expiration != null) {
+                long ttl = getSystemExpiration(expiration);
+                record.setExpiration(expiration);
+                this.client.getMapCache(context).fastPut(key, record, ttl, TimeUnit.MILLISECONDS);
+            } else {
+                this.client.getMapCache(context).fastPut(key, record);
+            }
+            return record.getVersion();
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
     public boolean update(@Nonnull String context, @Nonnull String key, @Nonnull String value, @Nullable Long expiration) throws IOException {
-        return false;
+        return doUpdate(null, context, key, value, expiration) != null;
     }
 
     @Nullable
     @Override
     public Long updateWithVersion(long version, @Nonnull String context, @Nonnull String key, @Nonnull String value, @Nullable Long expiration) throws IOException, VersionMismatchException {
-        return null;
+        try {
+            return doUpdate(version, context, key, value, expiration);
+        } catch (VersionMismatchWrapperException e) {
+            throw (VersionMismatchException)e.getCause();
+        }
     }
 
     @Override
     public boolean updateExpiration(@Nonnull String context, @Nonnull String key, @Nullable Long expiration) throws IOException {
-        return false;
+        return doUpdate(null, context, key, null, expiration) != null;
+    }
+
+    private boolean doDelete(Long version, String context, String key) {
+        RMapCache map = this.client.getMapCache(context);
+        if (!map.containsKey(key)) {
+            return false;
+        }
+        if (version != null && ((StorageRecord)map.get(key)).getVersion() != version) {
+            throw new VersionMismatchWrapperException(new VersionMismatchException());
+        }
+        map.remove(key);
+        return true;
     }
 
     @Override
     public boolean delete(@Nonnull String context, @Nonnull String key) throws IOException {
-        return false;
+        return doDelete(null, context, key);
     }
 
     @Override
     public boolean deleteWithVersion(long version, @Nonnull String context, @Nonnull String key) throws IOException, VersionMismatchException {
-        return false;
+        try {
+            return doDelete(version, context, key);
+        } catch (VersionMismatchWrapperException e) {
+            throw (VersionMismatchException)e.getCause();
+        }
     }
 
     @Override
     public void reap(@Nonnull String context) throws IOException {
-
+        // handled internally
     }
 
     @Override
@@ -101,6 +150,12 @@ public class RedisStorageService extends AbstractStorageService {
 
     @Override
     public void deleteContext(@Nonnull String context) throws IOException {
+        this.client.getMapCache(context).destroy();
+    }
 
+    public static class VersionMismatchWrapperException extends RuntimeException {
+        public VersionMismatchWrapperException(Throwable cause) {
+            super(cause);
+        }
     }
 }
--- src/main/java/net/unicon/iam/shibboleth/storage/VersionMutableStorageRecord.java
+++ src/main/java/net/unicon/iam/shibboleth/storage/VersionMutableStorageRecord.java
@@ -1,8 +1,10 @@
-package net.unicon.iam.shibboleth.storage.jackson;
+package net.unicon.iam.shibboleth.storage;
 
 import org.opensaml.storage.MutableStorageRecord;
 
-public class VersionMutableStorageRecord extends MutableStorageRecord {
+import java.io.Serializable;
+
+public class VersionMutableStorageRecord extends MutableStorageRecord implements Serializable {
     public VersionMutableStorageRecord(String value, Long expiration, Long version) {
         super(value, expiration);
         super.setVersion(version);
